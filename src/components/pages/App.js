d928d64921757e79da8355cb25f4f207a288448d
dennyzain <abbasdenny24@gmail.com>
2022-05-19 20:37:00 +0700

    adding fontawesom for icon lib

--- src/components/pages/App.js
+++ src/components/pages/App.js
@@ -1,36 +1,33 @@
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 function App() {
   return (
     <>
       <header>
-        <div className="flex justify-between px-5 py-3 items-center">
-          <img src="/logo512.png" width={128} alt="logo" />
-          <p className="">
-            Hallo, <span className="text-cyan-600">Gadjian User</span>
-          </p>
-        </div>
+        <nav className="flex justify-between p-3 items-center">
+          <div className="flex">
+            <button type="button">
+              <FontAwesomeIcon icon={faBars} color="#444444" />
+            </button>
+            <img src="/logo512.png" alt="logo" className="w-28 pl-4" />
+          </div>
+          <img
+            src="/logo192.png"
+            alt="profile-user"
+            className="rounded-full w-[40px] object-contain bg-black"
+          />
+        </nav>
       </header>
       <main className="flex">
-        <aside className="w-72 px-5">
+        <aside className="w-72 px-5 hidden lg:block">
           <ul>
             <li>Beranda</li>
             <li>Personnel List</li>
             <li>Daily Attendance</li>
           </ul>
         </aside>
-        <section className="bg-[#eaeaea] h-screen w-full">
-          <div className="p-5 m-5 flex justify-between bg-white">
-            <div className="flex">
-              <h1>PERSONNEL LIST</h1>
-              <h3>List of Personnels</h3>
-            </div>
-            <div className="flex">
-              <form>
-                <input type="text" placeholder="Find Personnels" />
-              </form>
-              <button type="button">ADD PERSONNEL +</button>
-            </div>
-          </div>
-        </section>
+        <section className="bg-[#eaeaea] h-screen w-full"></section>
       </main>
     </>
   );
